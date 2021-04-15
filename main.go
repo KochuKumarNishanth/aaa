10ece54243c43d8b57e94018a05ff233a69978e6
Kochu Kumar <nishanth.kochu.kumar@accenture.com>
2021-04-15 12:12:42 +0530

    commit after tutorial
    Signed-off-by: Kochu Kumar <nishanth.kochu.kumar@accenture.com>

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("asdasd")
-  
+	fmt.Println("asdasd")
 	fmt.Println("new line added")
+	fmt.Println("line 2 added")
+	fmt.Println("line 3 added")
+
 }
